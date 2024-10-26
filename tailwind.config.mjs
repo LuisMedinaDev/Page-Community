a3a491602cd0560453763acfff8edc5db02a57e2
Luis Medina <luisdev002@gmail.com>
2024-10-26 03:58:48 -0400

    medias clamp()

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,11 +6,7 @@ export default {
 			colors:{
 				'color-primario': '#dfe2ea'
 			},
-			backgroundImage:{
-				'hero-principal': 'url(public/images/Hero-p.webp)',
-				'hero-principal-2': 'url(public/images/Hero-p-2.png)',
-				
-			},
+		
 			backgroundSize: {
 		     	
 				'100%': '100%',
@@ -20,6 +16,7 @@ export default {
 				'custom': '0 0 40px #4e08ffc0', 
 				'custom2': '0 0 40px #bd08ffc0',
 			  },
+			  
 		},
 	},
 	plugins: [],
